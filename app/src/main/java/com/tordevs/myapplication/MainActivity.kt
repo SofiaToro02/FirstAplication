4b7e16486f72e9152e0263643659f00fb2313745
SofiaToro02 <20toro.sofia@gmail.com>
2021-11-10 16:54:18 -0500

    Parte uno de la Interfaz

--- app/src/main/java/com/tordevs/myapplication/MainActivity.kt
+++ app/src/main/java/com/tordevs/myapplication/MainActivity.kt
@@ -3,10 +3,10 @@ package com.tordevs.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() {//clase herencia :
+    override fun onCreate(savedInstanceState: Bundle?) {//metodo
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)//configureme el contenido de la vista(numero hexadecimal R es de reosurce layput backage activity_main)
     }
 }
 //config develop
